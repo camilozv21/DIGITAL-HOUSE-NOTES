48aa3b8e07cbc8a1245828256b56d557e39afcbc
camilozv21 <camilozv21@gmail.com>
2023-04-02 10:17:35 -0500

    Tipos de variables

---
+++ tipo_variables/App.js
@@ -0,0 +1,6 @@
+let nombre = "juan ",apellido = "zapata", edad = 21, numeroTelefono = "3045706775", socio = true, fechaPartido = "2023/02/22", horaPartido = "12:30", numeroSilla = 46, precioBoleto = 450000
+
+
+console.log(nombre, apellido,edad,socio);
+
+console.log(typeof(socio));
---
+++ tipo_variables/micro2.js
@@ -0,0 +1,9 @@
+let nombre = "David", apellido = "Zapata", sueldoActual = 25000, porcentaAumento = 10;
+
+let calculoAmento = (sueldoActual * porcentaAumento)/100;
+
+let nuevoSueldo = calculoAmento + sueldoActual;
+
+console.log("Hola, estimad@ ",nombre,apellido,"\nEn base a su sueldo actual: ");
+console.log(sueldoActual,"\nha recibido un aumento del: ",porcentaAumento,"%: ");
+console.log(calculoAmento,"\ny su nuevo sueldo es de: ", nuevoSueldo);
