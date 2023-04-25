f939ff034e8516c0eebdcfbd8cc299076a79ca79
camilozv21 <camilozv21@gmail.com>
2023-04-25 17:27:52 -0500

    Distintas clases

--- MODULO_4_ANATOMIA_SITIO_WEB/Agregando_estilos/clase1/app.js
+++ MODULO_4_ANATOMIA_SITIO_WEB/Agregando_estilos/clase1/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3030, () => console.log("Se levantó els ervidor en el puerto 3030 🚀"));
+app.listen(3000, () => console.log("Se levantó els ervidor en el puerto 3000 🚀"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
